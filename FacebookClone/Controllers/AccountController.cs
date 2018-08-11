beda36136e14240b6dc02ba01a69164c0fbf189b
dima9917 <dima9917@gmail.com>
2018-08-11 11:01:38 -0700

    S2L6

---
+++ FacebookClone/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FacebookClone.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            // Confirm user is not logged in
+            string username = User.Identity.Name;
+
+            if (!string.IsNullOrEmpty(username))
+                return Redirect("~/" + username);
+
+            // Return view
+            return View();
+        }
+    }
+}
