fcde52cefc137eaf692ebc70d925c36f367b8e3e
dima9917 <dima9917@gmail.com>
2018-08-11 10:06:25 -0700

    commit

---
+++ FacebookClone/EchoHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace FacebookClone
+{
+    public class EchoHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ FacebookClone/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(FacebookClone.Startup))]
+
+namespace FacebookClone
+{
+    [HubName("echo")]
+    public class Startup
+    {
+        
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
